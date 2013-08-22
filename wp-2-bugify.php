763a63d22485681b3c0de45036994690be7ee5c0
Michael Garde <mgarde@gmail.com>
2013-08-22 12:35:59 -0400

    Setting up menu

--- wp-2-bugify.php
+++ wp-2-bugify.php
@@ -9,3 +9,32 @@ Author URI: http://philipjoyner.com/
 Updated: 2013-06-19
 License: MIT
 */
+
+class bugify {
+	function __construct() {
+		add_action('admin_menu', array(&$this, 'bugify_admin_menu'));
+	}
+	function bugify_admin_menu() {
+		add_menu_page(		'Submit to Bugify',
+							'Bugify',
+							'manage_options',
+							'bugify',
+							array($this, 'bugify_view_submit'),
+							plugins_url( 'WP-2-Bugify/img/bugify-logo-medium.png' ),
+							110 );
+
+		add_submenu_page(	'bugify',
+							'Bugify Options',
+							'Options',
+							'manage_options',
+							'bugify-options',
+							array($this, 'bugify_view_options') );
+	}
+	function bugify_view_submit() {
+		require_once('views/submit.php');
+	}
+	function bugify_view_options() {
+		require_once('views/options.php');
+	}
+}
+$bugify = new bugify;
